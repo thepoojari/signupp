c8afb1a2d6a290c08127f18201bcc4e7f8e57d8b
thepoojari <93807424+thepoojari@users.noreply.github.com>
2021-12-08 12:35:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("a").addEventListener("click",movingtologin);
+function movingtologin()
+{
+alert("Press OK to move for Login page");
+document.querySelector("a").attr("href","https://thepoojari.github.io/logi/")
+
+}
